9f32db4dcfc29312a89c4a2123052e0c9a16be55
prudhvinadh007 <163080374+prudhvinadh007@users.noreply.github.com>
2024-05-30 12:39:57 +0530

    commit 10

--- leetcode/array/MinCostClimbingStairs.java
+++ leetcode/array/MinCostClimbingStairs.java
@@ -9,6 +9,7 @@
 //Example 2:
 //Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
 //Output: 6
+
 //Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
 //Note:
 //cost will have a length in the range [2, 1000].
